ea47208205da15f5a92e471e6497ce4f2bce2dfd
Sam Daulton <sdaulton@g.harvard.edu>
2016-11-22 14:46:51 -0500

    added figures for different numbers of cars

--- code/two_car_general_motors_example.py
+++ code/two_car_general_motors_example.py
@@ -13,10 +13,10 @@ def leadcar_problem1(t):
 	# 
 	if t >= 2 and t < 4:
 		# accelerate by 1.0 m/s^2
-		return 1.0
+		return 4.0
 	elif t >= 4 and t <= 6:
 		# accelerate by -1.0 m/s^2
-		return -1.0
+		return -4.0
 	else:
 		# t < 2 or t > 6
 		return 0.0
@@ -33,21 +33,26 @@ def main():
 	'''
 
 	# set number of cars
-	n = 2
-	# set initial pos, velocity, accel
-	x0 = np.array([0.0,28.0])
-	v0 = np.array([16.0,16.0])
-	a0 = np.array([0.0,0.0])
-	# set lead car acceleration function
-	lead_car_accel = leadcar_problem1
-	# create model
-	model = GeneralMotorsModel(n,x0,v0,a0,lead_car_accel)
-	# run simulation
-	model.run_sim()
-	# plot a,x,v of the cars
-	model.plot('v')
-	model.plot('x')
-	model.plot('a')
+	ns = [2,3,10,40] # number of cars
+	max_ts = [20.0,20.0,100.0,100.0] # to see effect for different number of cars
+	for j in xrange(len(ns)):
+		n = ns[j]
+		max_t = max_ts[j]
+		# set initial pos, velocity, accel
+		x0 = np.array([28.0*i for i in xrange(n)])
+		v0 = np.ones(n)*16.0
+		a0 = np.zeros(n)
+		
+		# set lead car acceleration function
+		lead_car_accel = leadcar_problem1
+		# create model
+		model = GeneralMotorsModel(n,x0,v0,a0,lead_car_accel,max_t=max_t)
+		# run simulation
+		model.run_sim()
+		# plot a,x,v of the cars
+		model.plot('v')
+		model.plot('x')
+		model.plot('a')
 
 
 if __name__ == '__main__':
