1e80ee5357b53e960db6a56e0730b699645620a6
Nick Hoernle <nick@intellectionsoftware.com>
2016-12-01 11:15:16 -0500

    IDM up and that weird first car bug fixed

--- code/another_model.py
+++ code/another_model.py
@@ -15,8 +15,7 @@ b = 3.
 delta = 4.
 s0 = 2.
 end_of_track = 600
-t_steps = 100000
-
+t_steps = 10000
 
 def x_dash(t, v):
 	return v
@@ -26,6 +25,9 @@ def x(x0, v, t):
 
 def s_star(v, delta_v):
 	return s0 + v*T + (v*delta_v/(2*np.sqrt(a*b)))**2
+
+def intelligent_driver_model_1(v, a, v0, delta, delta_v, s):
+	return np.concatenate(([v], [a*(1-(v/v0)**delta - (s_star(v, delta_v)/s)**2)]), axis=0)
 
 def x_v_dash(x_v, t):
 	# pdb.set_trace()
@@ -37,18 +39,16 @@ def x_v_dash(x_v, t):
 	s = x_vec.copy()
 
 	for i in range(len(s)):
+		# follow the leader
 		if i == len(s)-1:
 			delta_v[i] = v[0] - v[-1]
-			s[i] =  -end_of_track%(x_vec[0] - x_vec[-1])
+			s[i] = x_vec[-1] - x_vec[0] - end_of_track
 		else:
 			delta_v[i] =  v[i] - v[i+1]
 			s[i] =  x_vec[i] - x_vec[i+1]
-	# print x_vec
-	# print s
-	# print delta_v
-	# if (x_vec[-1] < 0.1): 
-	# pdb.set_trace()
-	x_v = np.concatenate(([v], [a*(1-(v/v0)**delta - (s_star(v, delta_v)/s)**2)]), axis=0).reshape(1,-1)[0]
+
+	# the magic happens here. We have
+	x_v = intelligent_driver_model_1(v, a, v0, delta, delta_v, s).reshape(1,-1)[0]
 	return x_v
 
 v = np.ones(50) * 30
@@ -60,15 +60,9 @@ ts = np.linspace(0,1000,t_steps)
 y_s = sp.integrate.odeint(x_v_dash, y0=x_v_vec, t=ts)
 
 fig, ax = plt.subplots(1,1, figsize=(8,8))
-# pdb.set_trace()q
-y_s = y_s.reshape(t_steps,-2,2)
-# for car in range(50):
-# 	ax.plot(ts, x_vec)
+
 for car in range(50):
-	# pdb.set_trace()
-	print y_s[:,car,0]
-	ax.plot(ts, y_s[:,car,0])
-# print xs
+	ax.plot(ts, y_s[:,car])
 plt.show()
 
 
