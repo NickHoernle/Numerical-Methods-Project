8abb560e1dd6e35c985a9663b9395d8c0159c005
Nick Hoernle <nick@intellectionsoftware.com>
2016-12-02 15:11:28 -0500

    added some fancy simulation

--- code/intelligent_driver_model.py
+++ code/intelligent_driver_model.py
@@ -5,6 +5,7 @@ import scipy as sp
 import scipy.integrate
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import pdb
 
 v0 = 30.
@@ -15,6 +16,7 @@ delta = 4.
 s0 = 2.
 end_of_track = 600
 t_steps = 10000
+n_cars = 50
 
 def x_dash(t, v):
 	return v
@@ -58,8 +60,48 @@ ts = np.linspace(0,1000,t_steps)
 
 y_s = sp.integrate.odeint(x_v_dash, y0=x_v_vec, t=ts)
 
-fig, ax = plt.subplots(1,1, figsize=(8,8))
+fig, axes = plt.subplots(1,2, figsize=(16,8))
 
 for car in range(50):
-	ax.plot(ts, y_s[:,car])
+	axes[0].plot(ts, y_s[:,car])
+
+for car in range(50):
+	axes[1].plot(ts, y_s[:,car+n_cars])
+
+axes[0].set_xlabel('$time$')
+axes[1].set_xlabel('$position$')
+axes[0].set_ylabel('$time$')
+axes[1].set_ylabel('$position$')
+plt.show()
+
+
+## Run a simulation of sorts
+r = (end_of_track/(2*np.pi))
+
+fx = lambda x_vec: r*np.sin((x_vec/end_of_track)*2*np.pi)
+fy = lambda x_vec: r*np.cos((x_vec/end_of_track)*2*np.pi)
+
+fig, ax = plt.subplots()
+
+t = np.arange(0, end_of_track, dtype=np.float32)
+t_pos = np.linspace(0, 2*np.pi, n_cars)
+
+line, = ax.plot(fx(t), fy(t))
+cars = ax.scatter(fx(x_vec), fy(x_vec), c='g')
+
+def animate(i):
+	x_vec = y_s[i,0:50]
+	x_vec = np.remainder(x_vec, end_of_track)
+	new_pos = np.concatenate(([fx(x_vec)], [fy(x_vec)]), axis=0)
+	cars.set_offsets(new_pos.T)
+	return cars,
+
+def init():
+	return cars,
+
+ani = animation.FuncAnimation(fig, animate, range(10000), init_func=init, interval=25, blit=False)
+
+ax.set_ylabel('$x$')
+ax.set_ylabel('$y$')
+
 plt.show()
