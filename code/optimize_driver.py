29b0c63881b72d34c20a49d25377c827a607baa4
Sophie Hilgard <ashilgard@gmail.com>
2016-12-09 16:18:11 -0500

    running

--- code/optimize_driver.py
+++ code/optimize_driver.py
@@ -7,8 +7,9 @@ import matplotlib.animation as animation
 import pdb
 from intelligent_driver_model import *
 from human_driver_model import *
+import copy
 
-def runge_kutta_4_driverparams(x_v_vec_k, x_v_dash, t_k, h, params, past, driveridx, driverparams):
+def runge_kutta_4_driverparams(x_v_vec_k, x_v_dash, t_k, h, params, driveridx, driverparams):
 	k1=x_v_dash(x_v_vec_k,t_k, params,driveridx, driverparams)
 	k2=x_v_dash(x_v_vec_k+.5*h*k1,t_k+.5*h, params,driveridx, driverparams)
 	k3=x_v_dash(x_v_vec_k+.5*h*k2,t_k+.5*h, params,driveridx, driverparams)
@@ -42,23 +43,25 @@ def x_v_dash_driver(x_v, t, params, driveridx, driverparams):
 
 	# s[0] = float('inf')
 	# Compute acceleration
+	# pdb.set_trace()
 	if params['IDM_model_num'] == 0:
 		# Standard IDM
 		dvdt = a_IDM(v,s,delta_v,params)
-        dvdt_2 = a_IDM(v,s,delta_v,driverparams)
-        dvdt[driveridx] = dvdt_2[driveridx]
+		# pdb.set_trace()
+		dvdt_2 = a_IDM(v,s,delta_v,driverparams)
+		dvdt[driveridx] = dvdt_2[driveridx]
 	else:
 		# compute Improved IDM acceration function
 		# Note: we compute this acceleration function for both IIDM and ACC
 		# For ACC, this serves as dvdt
 		dvdt = a_IIDM(v,s,delta_v,params)
-        dvdt_2 = a_IIDM(v,s,delta_v,driverparams)
-        dvdt[driveridx] = dvdt_2[driveridx]
+		dvdt_2 = a_IIDM(v,s,delta_v,driverparams)
+		dvdt[driveridx] = dvdt_2[driveridx]
 	if params['IDM_model_num'] == 2:
 		# AAC IDM
 		dvdt = a_ACC(s, v, vl, dvdt, params)
-        dvdt_2 = a_ACC(s, v, vl, dvdt,driverparams)
-        dvdt[driveridx] = dvdt_2[driveridx]
+		dvdt_2 = a_ACC(s, v, vl, dvdt,driverparams)
+		dvdt[driveridx] = dvdt_2[driveridx]
 	# if t <= 200:
 	# 	v[0] = 15
 	# if t > 200:
@@ -102,8 +105,8 @@ def x_v_dash_driver_human(x_v, t,params,driveridx, driverparams):
 	delta_v_est = v - v_l_est
 	# Compute acceleration (with estimation error)
 	dvdt = a_IDM(v,s_est,delta_v_est,params) + sigma_a*w_a[:, index]
-    dvdt_2 = a_IDM(v,s_est,delta_v_est,driverparams) + sigma_a*w_a[:, index]
-    dvdt[driveridx] = dvdt_2[driveridx]
+	dvdt_2 = a_IDM(v,s_est,delta_v_est,driverparams) + sigma_a*w_a[:, index]
+	dvdt[driveridx] = dvdt_2[driveridx]
 	x_v = np.concatenate((v,dvdt))
 	return x_v
 
@@ -167,59 +170,80 @@ def x_v_dash2_driver_human(x_v, t,params,past,driveridx, driverparams):
 		# calculate the acceleration of each vehicle one at a time
 		# As written in Eq 12.20 of the textbook
 		for alpha in xrange(n_cars):
-            if alpha == driveridx:
-                free_term_alpha = free_term[alpha]
-    			int_term = 0.0
-    			v_alpha_prog = np.array([v_prog[alpha]])
-    			for beta in xrange(alpha-n_a,alpha):
-    				# we want to sum from beta+1 to alpha
-    				# precompute these indices to avoid slicing issues
-    				sum_idxs = np.arange(beta+1,alpha+1)
-    				#print sum_idxs
-    				s_alpha_beta_prog = np.array([np.sum(s_prog[sum_idxs])])
-    				#print s_alpha_beta_prog
-    				v_beta_prog = np.array([v_l_prog[beta]])
-    				int_term += a_IDM_int(v_alpha_prog,s_alpha_beta_prog,v_alpha_prog-v_beta_prog,driverparams)
-    			dvdt[alpha] = free_term_alpha + c_idm*int_term
-            else:
-    			free_term_alpha = free_term[alpha]
-    			int_term = 0.0
-    			v_alpha_prog = np.array([v_prog[alpha]])
-    			for beta in xrange(alpha-n_a,alpha):
-    				# we want to sum from beta+1 to alpha
-    				# precompute these indices to avoid slicing issues
-    				sum_idxs = np.arange(beta+1,alpha+1)
-    				#print sum_idxs
-    				s_alpha_beta_prog = np.array([np.sum(s_prog[sum_idxs])])
-    				#print s_alpha_beta_prog
-    				v_beta_prog = np.array([v_l_prog[beta]])
-    				int_term += a_IDM_int(v_alpha_prog,s_alpha_beta_prog,v_alpha_prog-v_beta_prog,params)
-    			dvdt[alpha] = free_term_alpha + c_idm*int_term
+			if alpha == driveridx:
+				free_term_alpha = free_term[alpha]
+				int_term = 0.0
+				v_alpha_prog = np.array([v_prog[alpha]])
+				for beta in xrange(alpha-n_a,alpha):
+					# we want to sum from beta+1 to alpha
+					# precompute these indices to avoid slicing issues
+					sum_idxs = np.arange(beta+1,alpha+1)
+					#print sum_idxs
+					s_alpha_beta_prog = np.array([np.sum(s_prog[sum_idxs])])
+					#print s_alpha_beta_prog
+					v_beta_prog = np.array([v_l_prog[beta]])
+					int_term += a_IDM_int(v_alpha_prog,s_alpha_beta_prog,v_alpha_prog-v_beta_prog,driverparams)
+				dvdt[alpha] = free_term_alpha + c_idm*int_term
+			else:
+				free_term_alpha = free_term[alpha]
+				int_term = 0.0
+				v_alpha_prog = np.array([v_prog[alpha]])
+				for beta in xrange(alpha-n_a,alpha):
+					# we want to sum from beta+1 to alpha
+					# precompute these indices to avoid slicing issues
+					sum_idxs = np.arange(beta+1,alpha+1)
+					#print sum_idxs
+					s_alpha_beta_prog = np.array([np.sum(s_prog[sum_idxs])])
+					#print s_alpha_beta_prog
+					v_beta_prog = np.array([v_l_prog[beta]])
+					int_term += a_IDM_int(v_alpha_prog,s_alpha_beta_prog,v_alpha_prog-v_beta_prog,params)
+				dvdt[alpha] = free_term_alpha + c_idm*int_term
 	else:
 		# Compute acceleration (with estimation error)
 		dvdt = a_IDM(v,s_est,delta_v_est,params) + sigma_a*w_a[:, index]
-        dvdt_2 = a_IDM(v,s_est,delta_v_est,driverparams) + sigma_a*w_a[:, index]
-        dvdt[driveridx] = dvdt_2[driveridx]
+		dvdt_2 = a_IDM(v,s_est,delta_v_est,driverparams) + sigma_a*w_a[:, index]
+		dvdt[driveridx] = dvdt_2[driveridx]
 	x_v = np.concatenate((v,dvdt))
 	past['past_dvdt'].append(dvdt)
 	return x_v
 
+def run_idm_simulation(params, driveridx, driverparams):
+	v = np.ones(params['n_cars']) * params['init_v']
+	# Assign initial positions
+	x_vec = np.linspace(0,params['end_of_track']-params['end_of_track']/6,params['n_cars'])
+	# reverse positions so that car 0 is leading
+	x_vec = x_vec[::-1]
+	# create 1D vector of positions followed by velocities
+	x_v_vec = np.concatenate(([x_vec], [v]), axis=0).reshape(1,-1)[0]
+	# time
+	ts = np.linspace(0,params['total_time'],params['t_steps'])
+	# Solve System of ODEs
+	#params['y_s'] = [x_v_vec]
+	#y_s = sp.integrate.odeint(x_v_dash, y0=x_v_vec, t=ts, args=(params,))
+	y_s=[]
+	y_s.append(x_v_vec)
+	for i in range(1,len(ts)):
+		y_s.append(runge_kutta_4_driverparams(y_s[-1], x_v_dash_driver, ts[i], ts[i]-ts[i-1], params,driveridx, driverparams))
+	y_s = np.array(y_s)
+
+	return y_s
+
 # define a wrapper function to determine what cost is being calculated
-# def cost_wrapper(optimize_params, cost_func, optimize_variables, params):
-# 	for var, param in zip(optimize_variables, optimize_params):
-# 		params[var] = param
-#
-# 	simulation_result = intelligent_driver_model.run_simulation(params)
-# 	# pdb.set_trace()
-# 	x_v_dash = np.array(params['x_v_dash'])
-#
-# 	displacement = simulation_result[:,:params['n_cars']].T
-# 	velocity = simulation_result[:,params['n_cars']:].T
-# 	acceleration = x_v_dash[:,params['n_cars']:].T
+def cost_wrapper(optimize_params, cost_func, optimize_variables, params, driveridx, driverparams):
+	for var, param in zip(optimize_variables, optimize_params):
+		driverparams[var] = param
+
+	simulation_result = run_idm_simulation(params, driveridx, driverparams)
+	# pdb.set_trace()
+	x_v_dash = np.array(params['x_v_dash'])
+
+	displacement = simulation_result[:,:params['n_cars']].T
+	velocity = simulation_result[:,params['n_cars']:].T
+	acceleration = x_v_dash[:,params['n_cars']:].T
 #
 # 	# We should be able to write cost functions based on these three variables
-# 	c = cost_func(displacement, velocity, acceleration, params)
-# 	return c
+	c = cost_func(displacement, velocity, acceleration, params, driveridx)
+	return c
 #
 # # Define the cost functions:
 #
@@ -227,8 +251,8 @@ def x_v_dash2_driver_human(x_v, t,params,past,driveridx, driverparams):
 # # Minimizing the total travel time -> Page 419
 # # Note: Minimising the travel time is the same as maximising the
 # # displacement across the time interval
-# def maximise_displacement(displacement, velocity, acceleration, params):
-# 	return 1-np.sum(displacement[:,-1])/float(params['n_cars'])
+def maximise_displacement(displacement, velocity, acceleration, params,driveridx):
+	return 1-displacement[driveridx,-1]
 #
 # # Cost Function 2
 # # Maximizing the driving comfort (minimise discomfort) -> Page 419
@@ -265,7 +289,7 @@ def x_v_dash2_driver_human(x_v, t,params,past,driveridx, driverparams):
 if __name__ == '__main__':
 	## Parameters ##
 	params = dict()
-	params['v0'] = 50.0 # desired velocity (in m/s) of vehicles in free traffic
+	params['v0'] = 30.0 # desired velocity (in m/s) of vehicles in free traffic
 	params['init_v'] = 5.0 # initial velocity
 	params['T'] = 2.5 # Safe following time
 	# Maximum acceleration (in m/s^2)
@@ -278,15 +302,15 @@ if __name__ == '__main__':
 	params['a'] = 1.0
 	params['T_idm'] = 1.
 	params['a_idm'] = 1.0
-	params['v0_idm'] = 50.0
+	params['v0_idm'] = 30.0
 	params['b'] = 3.0 # Comfortable deceleration (in m/s^2)
 	params['delta'] = 4.0 # Acceleration exponent
 	params['s0'] = 10.0 # minimum gap (in m)
 	params['end_of_track'] = 1000 # in m
-	params['t_steps'] = 1000 # number of timesteps
+	params['t_steps'] = 10000 # number of timesteps
 	params['t_start'] = 0.0
-	params['n_cars'] = 50 # number of vehicles
-	params['total_time'] = 300 # total time (in s)
+	params['n_cars'] = 10 # number of vehicles
+	params['total_time'] = 3000 # total time (in s)
 	params['c'] = 0.99 # correction factor
 	params['t_step'] = (params['total_time'] - params['t_start'])/params['t_steps']
 	params['Tr'] = .6 # Reaction time
@@ -320,65 +344,16 @@ if __name__ == '__main__':
 	params['w_a'] = wiener_process(tau_a_tilde, params)
 	params['j'] = int(Tr/t_step) # number of time steps in reaction time
 	params['r'] = Tr/t_step - params['j'] # fractional part of Tr/t_step
-	params['IDM_model_num'] = 2
+	params['IDM_model_num'] = 0
+	params['x_v_dash'] 			= [np.zeros(2*params['n_cars'])]
 	past = dict()
 	past['past_v_l_est_s'] = deque(maxlen=params['j']+2)
 	past['past_delta_v_est_s'] = deque(maxlen=params['j']+2)
 	past['past_dvdt'] = deque(maxlen=params['j']+2)
+	driverparams = copy.deepcopy(params)
 
-    solution = sp.optimize.minimize(cost_wrapper, [5.] , args=(combination_model, ['v0'], params), bounds=[(2., 500.)])
-	# solution = sp.optimize.minimize(cost_wrapper, [5.] , args=(maximise_displacement, ['v0'], params), bounds=[(2., 100.)])
+	for i in range(0,9):
+	# solution = sp.optimize.minimize(cost_wrapper, [5.] , args=(combination_model, ['v0'], params, 2, driverparams), bounds=[(2., 500.)])
+		solution = sp.optimize.minimize(cost_wrapper, [30.] , args=(maximise_displacement, ['v0'], params, i, driverparams), bounds=[(30., 300.)])
 
-	print solution.x
-
-	# IDM_pct = .3
-	# np.random.seed(0)
-	# indices = np.random.permutation(range(2,n_cars))
-	# IDM_idx = sorted(np.concatenate(([1,2],indices[:int(IDM_pct*n_cars)],[n_cars+1, n_cars+2], [i + n_cars for i in indices[:int(IDM_pct*n_cars)]]), axis=0))
-	# HDM_idx = sorted(np.concatenate(([0],indices[int(IDM_pct*n_cars):],[n_cars],[i + n_cars for i in indices[int(IDM_pct*n_cars):]]), axis=0))
-	# params['n_IDM_cars'] = int(IDM_pct*n_cars)
-	# params['n_HDM_cars'] = n_cars - int(IDM_pct*n_cars)
-    #
-	# v = np.ones(n_cars) * params['init_v']# Initial velocities (in m/s)
-	# x_vec = np.linspace(0,end_of_track-end_of_track/5,n_cars)	# Initial positions
-	# # reverse initial positions
-	# x_vec = x_vec[::-1]
-	# ts = np.linspace(t_start,total_time,t_steps) # time steps
-	# x_v_vec = np.concatenate(([x_vec], [v]), axis=0).reshape(1,-1)[0]
-	# # Runge Kutta isn't quite working, but I haven't looked into why
-	# y_s = []
-	# y_s.append(x_v_vec)
-	# #params['past_v_s'].append(v)
-	# for i in range(1,len(ts)):
-    #
-	# 	# y_next = runge_kutta_blended_4(y_s[-1], x_v_dash, x_v_dash2, ts[i], ts[i]-ts[i-1],IDM_idx, HDM_idx, params, past)
-	# 	# x_next = y_next[:n_cars]
-	# 	# v_next = y_next[n_cars:]
-	# 	# v_next[v_next<0]=0
-	# 	# y_next = np.concatenate((x_next, v_next), axis=0)
-	# 	# # y_next = runge_kutta_blended_5(y_s[-1], x_v_dash, x_v_dash2, ts[i], ts[i]-ts[i-1],IDM_idx, HDM_idx, params, past)
-	# 	# # pdb.set_trace()
-	# 	# y_s.append(y_next)
-    #     # runge_kutta_4_driverparams(x_v_vec_k, x_v_dash, t_k, h, params, driveridx, driverparams):
-	# 	y_s.append(runge_kutta_4_driverparams(y_s[-1], x_v_dash2, ts[i], ts[i]-ts[i-1],params,past, driveridx, driverparams))
-	# 	#params['past_v_s'].append(params['past_y_s'][-1][n_cars:])
-	# 	if i %200 == 0:
-	# 		print "Finished Iteration: i={}".format(i)
-	# y_s = np.array(y_s)
-	# #y_s = sp.integrate.odeint(x_v_dash, y0=x_v_vec, t=ts,args=(params,))
-    #
-	# # Plot position and velocity of each car
-	# fig, axes = plt.subplots(1,2, figsize=(16,8))
-	# # Plot positions over time
-	# for car in xrange(n_cars):
-	# 	axes[0].plot(ts, y_s[:,car])
-	# # Plot velocity over time
-	# for car in xrange(n_cars):
-	# 	axes[1].plot(ts, y_s[:,car+n_cars])
-	# axes[0].set_xlabel('Time')
-	# axes[0].set_ylabel('Position')
-	# axes[1].set_xlabel('Time')
-	# axes[1].set_ylabel('Velocity')
-	# plot_out_name = '../figures/blended_{}.png'.format(params['n_a'])
-	# plt.savefig(plot_out_name,
-	# 			orientation='landscape',format='png',edgecolor='black')
+		print solution.x
